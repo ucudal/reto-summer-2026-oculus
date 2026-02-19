6d9cb8558d26996d09b88c136e7eef58fe5c557f
Agustín Martínez <martinezagustin1215@gmail.com>
2026-02-19 10:26:51 -0300

    Timer

---
+++ Assets/Scripts/TimerScript.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using TMPro;
+
+public class TimerScript : MonoBehaviour
+{
+public float timeLeft = 60f; 
+public TextMeshProUGUI timerText; 
+
+
+void Update()
+{
+if (timeLeft > 0)
+{
+timeLeft -= Time.deltaTime;
+timerText.text = Mathf.Floor(timeLeft).ToString();
+}
+else
+{
+timeLeft = 0; 
+timerText.text = "0"; 
+Debug.Log("¡Tiempo terminado!"); 
